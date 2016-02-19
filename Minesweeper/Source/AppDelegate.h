56ab0477b33c3fd20f7528ca22808e7005f7ab67
Amita Pai <amitapai@amitas-macbook-pro.local>
2016-02-18 19:40:21 -0800

    .

---
+++ Minesweeper/Source/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Minesweeper
+//
+//  Created by Amita Pai on 2/17/16.
+//  Copyright © 2016 Amita Pai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
